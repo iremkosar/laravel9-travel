2305e57360da9f4749a91725dcfd067fb3e8fbbd
iremkosar <iremkosar94@gmail.com>
2022-04-23 17:51:33 +0300

    committ

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+@section('title','Category List')
+
+
+
+
+
+
+@section('content')
+    @include('admin._content')
+@endsection
+
+@section('footer')
+    @include('admin._footer')
+@endsection
